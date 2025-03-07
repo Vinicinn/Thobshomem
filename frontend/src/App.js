109dc7778be9077edac8446d63f35ef05e162279
Vinicius Melo <viniciusmelo554@gmail.com>
2025-03-07 10:39:02 -0300

    establish WebSocket connection using Socket.IO

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
 function App() {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket = io("https://thobshomem.onrender.com");
+    setSocket(newSocket);
+
+    return () => newSocket.close();
+  }, []);
+
   return <div className="App">hello world</div>;
 }
 
